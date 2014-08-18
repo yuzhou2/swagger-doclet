a2fe496b3bf65a5bc165ce04a25b1d56d33b05a1
Conor <conor.roche@avego.com>
2014-08-18 15:22:16 +0100

    Issue 9 committing dropwizard update for swagger ui 2.0.22 and test cases

---
+++ swagger-doclet/src/main/java/com/carma/swagger/doclet/translator/QualifiedType.java
@@ -0,0 +1,98 @@
+package com.carma.swagger.doclet.translator;
+
+import com.sun.javadoc.Type;
+
+/**
+ * The QualifiedType represents a type with a qualifier.
+ * @version $Id$
+ * @author conor.roche
+ */
+public class QualifiedType {
+
+	private final String qualifier;
+	private final Type type;
+	private final String typeName;
+
+	/**
+	 * This creates a QualifiedType
+	 * @param qualifier
+	 * @param type
+	 */
+	public QualifiedType(String qualifier, Type type) {
+		super();
+		this.qualifier = qualifier;
+		this.type = type;
+		this.typeName = type == null ? null : this.type.qualifiedTypeName();
+	}
+
+	/**
+	 * This creates a QualifiedType
+	 * @param type
+	 */
+	public QualifiedType(Type type) {
+		this(null, type);
+	}
+
+	/**
+	 * This gets the qualifier
+	 * @return the qualifier
+	 */
+	public String getQualifier() {
+		return this.qualifier;
+	}
+
+	/**
+	 * This gets the type
+	 * @return the type
+	 */
+	public Type getType() {
+		return this.type;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((this.qualifier == null) ? 0 : this.qualifier.hashCode());
+		result = prime * result + ((this.typeName == null) ? 0 : this.typeName.hashCode());
+		return result;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		QualifiedType other = (QualifiedType) obj;
+		if (this.qualifier == null) {
+			if (other.qualifier != null) {
+				return false;
+			}
+		} else if (!this.qualifier.equals(other.qualifier)) {
+			return false;
+		}
+		if (this.typeName == null) {
+			if (other.typeName != null) {
+				return false;
+			}
+		} else if (!this.typeName.equals(other.typeName)) {
+			return false;
+		}
+		return true;
+	}
+
+}
---
+++ swagger-doclet/src/test/resources/fixtures/fileupload/FileUploadResource.java
@@ -0,0 +1,106 @@
+package fixtures.fileupload;
+
+import java.io.File;
+import java.io.InputStream;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
+
+import com.sun.jersey.core.header.FormDataContentDisposition;
+import com.sun.jersey.multipart.FormDataParam;
+
+/**
+ * The FileUploadResource represents a resource for testing various flavours of file upload.
+ * @version $Id$
+ * @author conor.roche
+ */
+@Path("/fileupload")
+@SuppressWarnings("javadoc")
+public class FileUploadResource {
+
+	@POST
+	@Path("/jerseyformdata")
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	public Response jerseyFormData(@FormDataParam("file") InputStream fileInputStream,
+			@FormDataParam("contentDisposition") FormDataContentDisposition contentDispositionHeader) {
+		int fileSize = 0;
+		return Response.ok().entity("Received file of " + fileSize + " bytes.").build();
+	}
+
+	@POST
+	@Path("/multipartjavafile")
+	@Consumes({ MediaType.MULTIPART_FORM_DATA, MediaType.APPLICATION_OCTET_STREAM })
+	public Response multipartJavafile(File theFile) {
+		long fileSize = theFile.length();
+		return Response.ok().entity("Received file of " + fileSize + " bytes.").build();
+	}
+
+	@POST
+	@Consumes({ "image/*" })
+	@Path("/singlepartjavafile")
+	Response singlePartJavaFile(File theFile) {
+		// NOTE as this is NOT multipart it should be rendered in swagger as param type body
+		long fileSize = theFile.length();
+		return Response.ok().entity("Received file of " + fileSize + " bytes.").build();
+	}
+
+	@POST
+	@Path("/formparams")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public Response formParams(@FormParam("param1") String param1a, @FormParam("param2") String param2a) {
+		return Response.ok().entity("Received param1: " + param1a + ", param2: " + param2a).build();
+	}
+
+	// NOTE in carma we expect the image data to be in a part named photo, we could rename the
+	// method parameter from data to photo and that would work, however data is more correct
+	// and because it may be useful in other scenarios we use the new
+	// @paramsName option
+	/**
+	 * @paramsName data photo
+	 */
+	@POST
+	@Consumes({ MediaType.MULTIPART_FORM_DATA })
+	@Path("/resteasymultipartformdatainput")
+	// TODO ideally composite multipart types like MultipartFormDataInput and the CommonsMultipartFile
+	// can be treated similar to @BeanParam type classes where
+	// the related class can define the parts that should be submitted on upload as additional parameters...
+	Response resteasyMultipartFormDataInput(MultipartFormDataInput data) {
+		// List<InputPart> parts = data.getFormDataMap().get("photo");
+		return Response.ok().build();
+	}
+
+	@POST
+	@Consumes({ "image/*" })
+	@Path("/singlepartinputstream")
+	Response singlePartInputStream(InputStream photo) {
+		// NOTE as this is NOT multipart it should be rendered in swagger as param type body
+		return Response.ok().build();
+	}
+
+	@POST
+	@Consumes(MediaType.APPLICATION_OCTET_STREAM)
+	@Path("/bytearraysinglepart")
+	public Response byteArraySinglePart(byte[] payload) {
+		if (payload != null) {
+			return Response.ok().build();
+		}
+		return Response.status(400).build();
+	}
+
+	@POST
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	@Path("/bytearraymultipart")
+	public Response byteArrayMultiPart(byte[] payload) {
+		if (payload != null) {
+			return Response.ok().build();
+		}
+		return Response.status(400).build();
+	}
+
+}
