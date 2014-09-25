932042da140b4211e4d2c622bdbd6f36c0cd22be
conorroche <conor.roche@avego.com>
2014-09-25 20:26:41 +0100

    revising testcase for issue 17

--- swagger-doclet/src/test/resources/fixtures/issue17/Resource.java
+++ swagger-doclet/src/test/resources/fixtures/issue17/Resource.java
@@ -1,7 +1,9 @@
 package fixtures.issue17;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
 
 @Path("/issue17")
 @SuppressWarnings("javadoc")
@@ -10,5 +12,10 @@ public class Resource {
 	@GET
 	public User getUser() {
 		return new User();
+	}
+
+	@POST
+	public Response createUser(User2 user) {
+		return Response.ok().build();
 	}
 }
