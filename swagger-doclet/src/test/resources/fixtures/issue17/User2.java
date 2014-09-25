f56c436e2f8000f4054b2ad317e148c1b3c95fa8
conorroche <conor.roche@avego.com>
2014-09-25 20:28:54 +0100

    revising testcase for issue17

---
+++ swagger-doclet/src/test/resources/fixtures/issue17/User2.java
@@ -0,0 +1,33 @@
+package fixtures.issue17;
+
+import javax.xml.bind.annotation.XmlElement;
+
+/**
+ * The User represents an example that uses xml element and attributes to customize field name
+ * @version $Id$
+ * @author conor.roche
+ */
+@SuppressWarnings("javadoc")
+public class User2 {
+
+	private String username;
+	private String firstName;
+
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	@XmlElement(name = "first_name")
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+}
