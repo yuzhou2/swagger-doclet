557bb8135ab2d1f20757bd9fa2387641db17a72b
conorroche <conor.roche@avego.com>
2014-12-11 11:10:30 +0000

    extending test case for issue 30

---
+++ swagger-doclet/src/test/resources/fixtures/maps/ModelC.java
@@ -0,0 +1,12 @@
+package fixtures.maps;
+
+import java.util.Map;
+
+@SuppressWarnings("javadoc")
+public class ModelC {
+
+	public Map<String, Map<String, String>> getData() {
+		return null;
+	}
+
+}
---
+++ swagger-doclet/src/test/resources/fixtures/maps/ModelD.java
@@ -0,0 +1,17 @@
+package fixtures.maps;
+
+import java.util.Map;
+import java.util.Set;
+
+@SuppressWarnings("javadoc")
+public class ModelD {
+
+	public Map<String, Set<String>> getData() {
+		return null;
+	}
+
+	public Map<String, Map<String, String>> getData2() {
+		return null;
+	}
+
+}
