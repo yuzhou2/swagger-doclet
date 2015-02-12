5a12094825e559d4691697e5557af9111c90d502
conorroche <conor.roche@avego.com>
2015-02-12 20:48:59 +0000

    issue 49 support for options, head, patch methods

--- swagger-doclet/src/main/java/com/carma/swagger/doclet/model/HttpMethod.java
+++ swagger-doclet/src/main/java/com/carma/swagger/doclet/model/HttpMethod.java
@@ -3,33 +3,48 @@ package com.carma.swagger.doclet.model;
 import com.sun.javadoc.AnnotationDesc;
 import com.sun.javadoc.MethodDoc;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public enum HttpMethod {
-    GET("javax.ws.rs.GET"),
-    PUT("javax.ws.rs.PUT"),
-    POST("javax.ws.rs.POST"),
-    DELETE("javax.ws.rs.DELETE");
 
-    private final String canonicalClassname;
+	GET("javax.ws.rs.GET"),
+	PUT("javax.ws.rs.PUT"),
+	POST("javax.ws.rs.POST"),
+	DELETE("javax.ws.rs.DELETE"),
+	HEAD("javax.ws.rs.HEAD"),
+	OPTIONS("javax.ws.rs.OPTIONS"),
+	PATCH(".PATCH", true);
 
-    private HttpMethod(String canonicalClassname) {
-        this.canonicalClassname = canonicalClassname;
-    }
+	// NOTE Patch is not part of JAXRS 1 or 2 as it stands (people can add it but it will have an arbitrary package)
+	// so we will look for any annotation ending in .PATCH
 
-    public static HttpMethod fromMethod(MethodDoc method) {
-        List<String> typeNames = new ArrayList<String>();
-        for (AnnotationDesc annotation : method.annotations()) {
-            typeNames.add(annotation.annotationType().qualifiedTypeName());
-        }
-        HttpMethod found = null;
-        for (HttpMethod value : HttpMethod.values()) {
-            if (typeNames.contains(value.canonicalClassname)) {
-                found = value;
-                break;
-            }
-        }
-        return found;
-    }
+	private final String className;
+	private final boolean useContains;
+
+	private HttpMethod(String className) {
+		this.className = className;
+		this.useContains = false;
+	}
+
+	private HttpMethod(String className, boolean useContains) {
+		this.className = className;
+		this.useContains = useContains;
+	}
+
+	/**
+	 * This finds a HTTP method for the given method
+	 * @param method The java method to check
+	 * @return The HTTP method or null if there is not HTTP method annotation on the java method
+	 */
+	public static HttpMethod fromMethod(MethodDoc method) {
+		for (AnnotationDesc annotation : method.annotations()) {
+			String qName = annotation.annotationType().qualifiedTypeName();
+			for (HttpMethod value : HttpMethod.values()) {
+				if (value.useContains && qName.contains(value.className)) {
+					return value;
+				} else if (!value.useContains && qName.equals(value.className)) {
+					return value;
+				}
+			}
+		}
+		return null;
+	}
 }
