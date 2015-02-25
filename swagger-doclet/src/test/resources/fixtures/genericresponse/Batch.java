e5e9d9bd0aada89fc8bbe69ced1c1f50453d6339
conorroche <conor.roche@avego.com>
2015-02-25 00:52:20 +0000

    Issue 54 committeding test items

---
+++ swagger-doclet/src/test/resources/fixtures/genericresponse/Batch.java
@@ -0,0 +1,11 @@
+package fixtures.genericresponse;
+
+import java.util.Set;
+
+@SuppressWarnings("javadoc")
+public class Batch<T> {
+
+	Set<T> items;
+	String nextToken;
+
+}
---
+++ swagger-doclet/src/test/resources/fixtures/genericresponse/Item.java
@@ -0,0 +1,9 @@
+package fixtures.genericresponse;
+
+@SuppressWarnings("javadoc")
+public class Item {
+
+	String id;
+	String desc;
+
+}
