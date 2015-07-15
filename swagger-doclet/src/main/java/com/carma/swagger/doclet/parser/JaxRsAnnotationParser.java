c0b407f9d9d1dbc40b81821366e23f361b3ab1bf
conorroche <conor.roche@avego.com>
2015-07-15 11:44:11 +0100

    Issue 70 upgrading swagger ui to 2.1.0

--- swagger-doclet/src/main/java/com/carma/swagger/doclet/parser/JaxRsAnnotationParser.java
+++ swagger-doclet/src/main/java/com/carma/swagger/doclet/parser/JaxRsAnnotationParser.java
@@ -39,7 +39,7 @@ public class JaxRsAnnotationParser {
 	// diffs between 1.1 and 1.2 see https://github.com/wordnik/swagger-spec/wiki/1.2-transition
 	private static final String SWAGGER_VERSION = "1.2";
 
-	private static final String SWAGGER_UI_VERSION = "2.1.8-M1";
+	private static final String SWAGGER_UI_VERSION = "2.1.0";
 
 	private final DocletOptions options;
 	private final RootDoc rootDoc;
